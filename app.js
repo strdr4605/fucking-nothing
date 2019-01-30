630e8f9f5a8117d12c2a27e5c356e22e90070eac
TeeSeal <teesealz@gmail.com>
2019-01-30 21:33:24 +0200

    Add boilerplate

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+const { join } = require('path')
+
+app.use(express.static('assets'))
+app.use(express.static('js'))
+
+app.get('*', (_, res) => {
+  res.sendFile(join(__dirname, 'html', 'index.html'))
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}.`))
