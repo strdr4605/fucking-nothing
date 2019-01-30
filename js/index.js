d880ecfba491addc195460874d8dba75dcef3c3c
TeeSeal <teesealz@gmail.com>
2019-01-30 23:05:38 +0200

    Refactor JS

--- js/index.js
+++ js/index.js
@@ -1,20 +1,13 @@
 const audio = new Audio('fucking-nothing.mp3')
-
 const loli = document.getElementById('loli')
-const staticLoli = document.getElementById('static-loli')
 
 let playing = false;
-
-audio.onended = () => {
-  playing = false
-}
-
+audio.onended = () => playing = false
 document.addEventListener('click', () => {
-  if (!playing) {
-    playing = true
-    audio.play()
-    loli.classList.remove('animate')
-    void loli.offsetWidth;
-    loli.classList.add('animate')
-  }
+  if (playing) return
+  playing = true
+  audio.play()
+  loli.classList.remove('animate')
+  void loli.offsetWidth;
+  loli.classList.add('animate')
 })
