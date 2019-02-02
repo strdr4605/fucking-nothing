fc974d67053d0572173c52b5d36dcc7e7d11b33f
Dragoș Străinu <str.dr4605@gmail.com>
2019-02-02 15:45:01 +0200

    change README.md, add touchstart event for mobile users

--- assets/index.js
+++ assets/index.js
@@ -3,15 +3,17 @@
   window.onload = () => {
     const audio = new Audio('assets/fucking-nothing.mp3');
     const body = document.getElementsByTagName('body')[0];
-    let playing = false;
-    audio.onended = () => void (playing = false);
-    document.addEventListener('click', () => {
+    const startAnimationAndSound = () => {
       if (playing) return;
       playing = true;
       audio.play();
       body.classList.remove('animate');
       void (body.offsetWidth);
       body.classList.add('animate');
-    });
+    }
+    let playing = false;
+    audio.onended = () => void (playing = false);
+    document.addEventListener('click', startAnimationAndSound);
+    document.addEventListener('touchstart', startAnimationAndSound);
   }
 })();
