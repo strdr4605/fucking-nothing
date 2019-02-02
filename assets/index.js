fff3e1d920c9a0c18b23f0fff6973fae13c8f0e3
Dragoș Străinu <str.dr4605@gmail.com>
2019-02-02 15:22:12 +0200

    remove unused files, prepare for gh-pages

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   window.onload = () => {
-    const audio = new Audio('fucking-nothing.mp3');
+    const audio = new Audio('assets/fucking-nothing.mp3');
     const body = document.getElementsByTagName('body')[0];
     let playing = false;
     audio.onended = () => void (playing = false);
