8a1b61ac845cea68060e0f7cca17e18c00ed2d39
Dragoș Străinu <str.dr4605@gmail.com>
2019-02-02 14:05:01 +0200

    empty body

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-const audio = new Audio('fucking-nothing.mp3')
-const loli = document.getElementById('loli')
-
-let playing = false;
-audio.onended = () => playing = false
-document.addEventListener('click', () => {
-  if (playing) return
-  playing = true
-  audio.play()
-  loli.classList.remove('animate')
-  void loli.offsetWidth;
-  loli.classList.add('animate')
-})
+(function () {
+  'use strict';
+  window.onload = () => {
+    const audio = new Audio('fucking-nothing.mp3');
+    const body = document.getElementsByTagName('body')[0];
+    let playing = false;
+    audio.onended = () => void (playing = false);
+    document.addEventListener('click', () => {
+      if (playing) return;
+      playing = true;
+      audio.play();
+      body.classList.remove('animate');
+      void (body.offsetWidth);
+      body.classList.add('animate');
+    });
+  }
+})();
